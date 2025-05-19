eb93c1a5b65a863dbd6ca8c4514f7da8c5ecdc42
Daniel Cooper <daniel@14lines.com>
2025-05-19 20:03:17 +0100

    ✨Implement resource templates

--- lib/mcp/resource.rb
+++ lib/mcp/resource.rb
@@ -11,13 +11,71 @@ module FastMcp
   class Resource
     class << self
       attr_accessor :server
+      attr_reader :template_params
 
       # Define URI for this resource
       # @param value [String, nil] The URI for this resource
       # @return [String] The URI for this resource
       def uri(value = nil)
-        @uri = value if value
+        if value
+          @uri = value
+          # Check if URI contains template parameters
+          if value.include?('{') && value.include?('}')
+            @is_template = true
+            @template_params = value.scan(/\{([^\}]+)\}/).flatten
+
+            # Generate a regex pattern for matching this URI
+            escaped_uri = Regexp.escape(value)
+
+            @template_params.each do |param|
+              escaped_param = Regexp.escape("{#{param}}")
+              escaped_uri = escaped_uri.sub(escaped_param, '([^/]+)')
+            end
+
+            @uri_pattern = Regexp.new("^#{escaped_uri}$")
+          else
+            @is_template = false
+            @template_params = nil
+            @uri_pattern = nil
+          end
+        end
         @uri || (superclass.respond_to?(:uri) ? superclass.uri : nil)
+      end
+
+      # Check if this resource has a templated URI
+      # @return [Boolean] true if the URI contains template parameters
+      def templated?
+        @is_template || false
+      end
+
+      # Get the regex pattern for matching this URI
+      # @return [Regexp, nil] The pattern for matching this URI
+      def uri_pattern
+        @uri_pattern || (superclass.respond_to?(:uri_pattern) && superclass.templated? ? superclass.uri_pattern : nil)
+      end
+
+      # Create a new instance with the given params
+      # @param params [Hash] The parameters for this resource instance
+      # @return [Resource] A new resource instance
+      def with_params(params)
+        resource_class = Class.new(self)
+        resource_class.instance_variable_set(:@params, params)
+
+        resource_class.define_singleton_method(:instance) do
+          @instance ||= begin
+            instance = new
+            instance.instance_variable_set(:@params, params)
+            instance
+          end
+        end
+
+        resource_class
+      end
+
+      # Get the parameters for this resource instance
+      # @return [Hash] The parameters for this resource instance
+      def params
+        @params || {}
       end
 
       # Define name for this resource
@@ -26,6 +84,13 @@ module FastMcp
       def resource_name(value = nil)
         @name = value if value
         @name || (superclass.respond_to?(:resource_name) ? superclass.resource_name : nil)
+      end
+
+      alias original_name name
+      def name
+        return resource_name if resource_name
+
+        original_name
       end
 
       # Define description for this resource
@@ -47,12 +112,21 @@ module FastMcp
       # Get the resource metadata (without content)
       # @return [Hash] Resource metadata
       def metadata
-        {
-          uri: uri,
-          name: resource_name,
-          description: description,
-          mimeType: mime_type
-        }.compact
+        if templated?
+          {
+            uriTemplate: uri,
+            name: resource_name,
+            description: description,
+            mimeType: mime_type
+          }.compact
+        else
+          {
+            uri: uri,
+            name: resource_name,
+            description: description,
+            mimeType: mime_type
+          }.compact
+        end
       end
 
       # Load content from a file (class method)
@@ -89,6 +163,11 @@ module FastMcp
 
     include Singleton
 
+    # Initialize with instance variables
+    def initialize
+      @params = {}
+    end
+
     # URI of the resource - delegates to class method
     # @return [String, nil] The URI for this resource
     def uri
@@ -98,7 +177,7 @@ module FastMcp
     # Name of the resource - delegates to class method
     # @return [String, nil] The name for this resource
     def name
-      self.class.name
+      self.class.resource_name
     end
 
     # Description of the resource - delegates to class method
@@ -111,6 +190,12 @@ module FastMcp
     # @return [String, nil] The MIME type for this resource
     def mime_type
       self.class.mime_type
+    end
+
+    # Get parameters from the URI template
+    # @return [Hash] The parameters extracted from the URI
+    def params
+      @params || self.class.params
     end
 
     # Method to be overridden by subclasses to dynamically generate content
